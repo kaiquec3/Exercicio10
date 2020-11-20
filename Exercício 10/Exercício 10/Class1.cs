b2bbbac905940c1e65e1d1d127997d14a623286f
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:28:30 -0300

    Add files via upload

---
+++ Exercício 10/Exercício 10/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_10
+{
+    class Terreno
+    {
+        double @base;
+        double altura;
+        double area;
+
+        public void SetBase(double p)
+        {
+            @base = p;
+        }
+
+        public void SetAltura(double p)
+        {
+            altura = p;
+        }
+
+        public double GetBase()
+        {
+            return @base;
+        }
+
+        public double GetAltura()
+        {
+            return altura;
+        }
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        public double CalcularArea()
+        {
+            area = @base * altura;
+            return area;
+        }
+
+        public string DimensionarTerreno()
+        {
+            string k;
+
+            if(CalcularArea() > 100)
+            {
+                k = "grande";
+            }
+            else
+            {
+                k = "pequeno";
+            }
+            return k;
+        }
+    }
+}
---
+++ Exercício 10/Exercício 10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercício_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Terreno();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o valor da base: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetBase(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe o valor da altura: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetAltura(k);
+            }
+            while (k <= 0);
+
+            Console.WriteLine($"A área do terreno é {a1.CalcularArea()}\nTerreno {a1.DimensionarTerreno()}");
+        }
+    }
+}
